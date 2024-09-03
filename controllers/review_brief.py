c6ad996884afb31a7b43b744949f018c58dbfcc8
utsavdas10 <97392403+utsavdas10@users.noreply.github.com>
2024-09-03 11:34:33 +0530

    flipkart scrapper added

--- controllers/review_brief.py
+++ controllers/review_brief.py
@@ -1,9 +1,11 @@
-from controllers.utils.scrap_reviews import scrap_reviews
+from controllers.utils.ScrapperModule import Scrapper
 from controllers.utils.classify_reviews import classify_reviews
 from controllers.utils.summarise_reviews import summarise_reviews
 
+scrapper = Scrapper()
+
 async def review_brief(url: str):
-    reviews =  await scrap_reviews(url)
+    reviews =  await scrapper.scrap_reviews(url)
     classified_reviews = await classify_reviews(reviews)
     summarised_reviews = await summarise_reviews(classified_reviews)
     return summarised_reviews
